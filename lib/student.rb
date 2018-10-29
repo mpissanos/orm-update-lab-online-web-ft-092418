192bd24923ae3e9c3ca6be2f20d9895b9f856264
Manny Pissanos <mpissano@gmail.com>
2018-10-29 15:46:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,88 @@
 require_relative "../config/environment.rb"
 
 class Student
+  
+  attr_accessor :name, :grade, :id
+  
+  
+  def initialize( id = nil, name, grade)
+   @name = name
+   @grade = grade
+   @id = nil
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id  INTEGER PRIMARY KEY,
+      name  TEXT,
+      grade  INTEGER)
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+        DROP TABLE IF EXISTS students
+      SQL
+      
+      DB[:conn].execute(sql)
+  end
+  
+  def save
+   if self.id
+    self.update
+  else
+    sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
+  
+  def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0] 
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      Limit 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new_from_db(result[0], result[1], result[2])
+  end
+    
+        
+      
+  
 end
